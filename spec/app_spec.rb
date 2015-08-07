c8cbfd028c1bf407e71d0580d24dc99511e15589
Lars Vonk <lars.vonk@gmail.com>
2015-08-07 13:34:58 +0200

    Better error handling for missing config

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,22 +4,46 @@ require 'rack/test'
 describe Sequent::Web::Sinatra::App do
   include Rack::Test::Methods
 
-  class MockSequentApp < Sinatra::Base
-    set :sequent_config_dir, "#{root}/support"
+  describe "sequent_config_dir" do
+    class MockOrderSequentApp < Sinatra::Base
+      set :root, "#{root}/support"
 
-    register Sequent::Web::Sinatra::App
+      register Sequent::Web::Sinatra::App
 
-    get '/' do
-      @command_service.class.to_s
+      get '/' do
+        @command_service.class.to_s
+      end
     end
+
+    def app
+      MockOrderSequentApp.new
+    end
+
+    it "should be optional" do
+      response = get '/'
+      expect(response.body).to eq('Sequent::Core::CommandService')
+    end
+
   end
 
-  def app
-    MockSequentApp.new
-  end
+  describe "initializing" do
+    class MockSequentApp < Sinatra::Base
+      set :sequent_config_dir, "#{root}/support"
 
-  it "should set @command_service on every request" do
-    response = get '/'
-    expect(response.body).to eq('Sequent::Core::CommandService')
+      register Sequent::Web::Sinatra::App
+
+      get '/' do
+        @command_service.class.to_s
+      end
+    end
+
+    def app
+      MockSequentApp.new
+    end
+
+    it "should set @command_service on every request" do
+      response = get '/'
+      expect(response.body).to eq('Sequent::Core::CommandService')
+    end
   end
 end
